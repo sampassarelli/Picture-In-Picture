78206f9997da2a301c0f2b05fa4f0134a79c1823
sampassarelli <passarelli.sam@gmail.com>
2021-03-11 13:21:19 -0600

    demo completed

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
- 
+ const videoElement = document.getElementById('video')
+ const button = document.getElementById('button')
+
+ // Prompt user to select media stream, pass it to video element, then play
+
+ async function selectMediaStream() {
+   try {
+     const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+     videoElement.srcObject = mediaStream
+     videoElement.onloadedmetadata = () => {
+       videoElement.play()
+     }
+   } catch (error) {
+     console.log('Whoops, error here', error);
+   }
+ }
+
+ button.addEventListener('click', async () => {
+   // Disable Button
+   button.disabled = true
+   // Start Picture in Picture
+   await videoElement.requestPictureInPicture()
+   // Reset the button
+   button.disabled = false
+ })
+
+ // On Load
+ selectMediaStream()
